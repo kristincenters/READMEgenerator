3338bd4b670d283f4a5f4c7c312bef56bb94fa22
kcenters <kristincenters@gmail.com>
2020-04-10 08:33:31 -0500

    changed api code

--- index.js
+++ index.js
@@ -1,67 +1,142 @@
 require('dotenv').config();
 const fs = require('fs');
 const inquirer = require('inquirer');
-const util = require('util');
+//const util = require('util');
 const axios = require('axios');
-const getapi = require('./gitapi.js');
-const genMD = require('./buildMD');
-const queries = inquirer
-	.prompt([
-		{
-			type: 'input',
-			name: 'username',
-			message: 'What is your GitHub User Name?',
-		},
-		{
-			type: 'input',
-			name: 'project',
-			message: 'What is your name of your project?',
-		},
-		{
-			type: 'input',
-			name: 'description',
-			message: 'Describe your project?',
-		},
-		{
-			type: 'input',
-			name: 'install',
-			message: 'What command is needed to install dependencies?',
-		},
-		{
-			type: 'input',
-			name: 'usage',
-			message: 'What does the user need to know about using this app?',
-		},
-		{
-			type: 'checkbox',
-			message: 'License types?',
-			name: 'license',
-			choices: ['MIT', 'Creative Commons', 'GNU'],
-		},
-		{
-			type: 'input',
-			name: 'contribute',
-			message: 'How can the user contribute to this project?',
-		},
-		{
-			type: 'input',
-			name: 'test',
-			message: 'How can the user test this application',
-		},
-	])
-	.then((queries) => {
-		console.log(queries);
-		getapi
-			.getUser(queries.username)
-			.then((response) => {
-				genMD(queries, response.data);
-				const markdown = genMD(queries, response.data);
-				fs.writeFile('./newREADME.md', markdown, () => {
-					console.log('works!');
+//const getapi = require('./gitapi.js');
+//const genMD = require('./buildMD');
+inquirer.prompt([
+	{
+		type: 'input',
+		name: 'username',
+		message: 'What is your GitHub User Name?',
+	},
+	{
+		type: 'input',
+		name: 'project',
+		message: 'What is your name of your project?',
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: 'Describe your project?',
+	},
+	{
+		type: 'input',
+		name: 'install',
+		message: 'What command is needed to install dependencies?',
+	},
+	{
+		type: 'input',
+		name: 'usage',
+		message: 'What does the user need to know about using this app?',
+	},
+	{
+		type: 'checkbox',
+		message: 'License types?',
+		name: 'license',
+		choices: ['MIT', 'Creative Commons', 'GNU'],
+	},
+	{
+		type: 'input',
+		name: 'contribute',
+		message: 'How can the user contribute to this project?',
+	},
+	{
+		type: 'input',
+		name: 'test',
+		message: 'How can the user test this application',
+	},
+]);
+
+const gitapi = {
+	getUser(username) {
+		axios
+			.get(`https://api.github.com/users/${username}`, {
+				headers: { Authorization: `token ${process.env.GH_TOKEN}` },
+			})
+			.then(function (response) {
+				const username = res.username;
+				const image = response.data.avatar_url;
+				const email = response.data.email;
+				const project = response.project;
+				const description = response.description;
+				const install = response.install;
+				const usage = response.usage;
+				const license = response.license;
+				const contribute = response.contribute;
+				const test = response.test;
+
+				const markdown = `
+
+![Badge](https://img.shields.io/static/v1?label=GitHub_User&message=kristincenters&color=<COLOR>?style=plastic)
+			
+## ${project}
+			
+## Description
+			
+> ${description}
+			
+## Table of Contents
+			
+<details>
+<summary>Installation</summary>
+			
+> ${install}
+			
+</details>
+<br>
+<details>
+			
+<summary>Usage</summary>
+			
+> ${usage}
+			
+</details>
+<br>
+<details>
+			
+<summary>License</summary>
+			
+> ${license}
+			
+</details>
+<br>
+<details>
+			
+<summary>Contributors</summary>
+			
+> ${contribute}
+			
+</details>
+<br>
+<details>
+			
+<summary>Testing</summary>
+			  
+> ${test}
+			  
+</details>
+<br>
+			
+## Contact me with questions:
+
+#### ${username}
+			
+<img style="border-radius: 50%" src="${image}" alt="github headshot">
+			
+### Email: ${email}`;
+
+				fs.writeFile('newREADME.md', markdown, (err) => {
+					if (err) {
+						return console.log(err);
+					}
 				});
 			})
-			.catch((error) => {
+			.catch(function (error) {
 				console.log(error);
+
+				console.log('success!');
 			});
-	});
-module.exports = queries;
+	},
+};
--- newREADME.md
+++ newREADME.md
@@ -1,58 +1,57 @@
 
 
 ![Badge](https://img.shields.io/static/v1?label=GitHub_User&message=kristincenters&color=<COLOR>?style=plastic)
-
-## README Generator
-
+			
+## undefined
+			
 ## Description
-
-The application allows developers to craft a quality README document with ease. Once initialized the application functions as a template and will prompt you with questions. When complete and executed, it will dynamically generate a new README file for your next project.
-
+			
+> undefined
+			
 ## Table of Contents
-
+			
 <details>
-  <summary>Installation</summary>
-
-> npm i or install
-
+<summary>Installation</summary>
+			
+> undefined
+			
 </details>
 <br>
 <details>
-
-  <summary>Usage</summary>
-
-> Clone repo, secure git api token and install packages axios, inquirer and dotenv',
-
+			
+<summary>Usage</summary>
+			
+> undefined
+			
 </details>
 <br>
 <details>
-
-  <summary>License</summary>
-
-> MIT
-
-
+			
+<summary>License</summary>
+			
+> undefined
+			
 </details>
 <br>
 <details>
-
-  <summary>Contributors</summary>
-
-> Clone the reop and follow instructions
-
+			
+<summary>Contributors</summary>
+			
+> undefined
+			
 </details>
 <br>
 <details>
-
-  <summary>Testing</summary>
-  
-Forking the repo
-  
+			
+<summary>Testing</summary>
+			  
+> undefined
+			  
 </details>
 <br>
-
-## Contact me with questions
-
-<img style="border-radius: 50%" src="https://www.linkedin.com/in/kristincenters/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B68ZYpVSoTT6e0i3UOXuQ3A%3D%3D" alt="github headshot">
-
+			
+## Contact me with questions:
+			
+<img style="border-radius: 50%" src="https://avatars0.githubusercontent.com/u/5201511?v=4" alt="github headshot">
+			
 ### Email: kristincenters@gmail.com
